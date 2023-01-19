2357d320e100c87d23c536c3c505ae424d668a8c
Niclas Söderström <niclas.soderstrom@gmp-systems.com>
2023-01-19 20:51:48 +0100

    Change indentation

--- functions/src/scrape-quarteria.ts
+++ functions/src/scrape-quarteria.ts
@@ -8,79 +8,79 @@ interface CalendarDay {
 }
 
 async function getCalenderDays(page: Page): Promise<CalendarDay[]> {
-  return await page.$$eval('div[data-section-id="AVAILABILITY_CALENDAR_INLINE"] .notranslate', divs => {
-    return divs
-        .map(div => {
-          const dateAsString = div.getAttribute('data-testid') || ''
-              .replace('calendar-day-', '')
+    return await page.$$eval('div[data-section-id="AVAILABILITY_CALENDAR_INLINE"] .notranslate', divs => {
+        return divs
+            .map(div => {
+                const dateAsString = div.getAttribute('data-testid') || ''
+                    .replace('calendar-day-', '')
 
-          return {
-            date: Date.parse(dateAsString),
-            booked: JSON.parse(div.getAttribute('data-is-day-blocked') || ''),
-          }
-        })
-  })
+                return {
+                    date: Date.parse(dateAsString),
+                    booked: JSON.parse(div.getAttribute('data-is-day-blocked') || ''),
+                }
+            })
+    })
 }
 
 async function clickNextOnCalendar(page: Page): Promise<void> {
-  await page.click('div[data-section-id="AVAILABILITY_CALENDAR_INLINE"] div._qz9x4fc > button', {
-    delay: 1000,
-  })
+    await page.click('div[data-section-id="AVAILABILITY_CALENDAR_INLINE"] div._qz9x4fc > button', {
+        delay: 1000,
+    })
 }
 
 function removeDuplicates(calendarDays: CalendarDay[]) {
-  return calendarDays.filter((object, index, self) => {
-    return index === self.findIndex(day => day['date'] === object['date'])
-  })
+    return calendarDays.filter((object, index, self) => {
+        return index === self.findIndex(day => day['date'] === object['date'])
+    })
 }
 
 const scrapeQuarteira = async () => {
-  const bookings: string[] = []
-  let calendarDays: CalendarDay[] = []
+    const bookings: string[] = []
+    let calendarDays: CalendarDay[] = []
 
-  const browser = await puppeteer.launch({
-    headless: true,
-    timeout: 20000,
-    ignoreHTTPSErrors: true,
-  })
-
-  try {
-    functions.logger.info('Starting scrape')
-
-    const page = await browser.newPage()
-    await page.setViewport({width: 1280, height: 720})
-    await page.setUserAgent(
-        // eslint-disable-next-line max-len
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'
-    )
-
-    await page.goto('https://www.airbnb.com/rooms/647559808158441027', {
-      waitUntil: 'networkidle2',
+    const browser = await puppeteer.launch({
+        headless: true,
+        timeout: 20000,
+        ignoreHTTPSErrors: true,
     })
 
-    await clickNextOnCalendar(page)
-    await clickNextOnCalendar(page)
-    await getCalenderDays(page).then(days => calendarDays.push(...days))
+    try {
+        functions.logger.info('Starting scrape')
 
-    for (let i = 0; i < 9; i++) {
-      await clickNextOnCalendar(page)
-      await getCalenderDays(page).then(days => calendarDays.push(...days))
-      calendarDays = removeDuplicates(calendarDays)
+        const page = await browser.newPage()
+        await page.setViewport({width: 1280, height: 720})
+        await page.setUserAgent(
+            // eslint-disable-next-line max-len
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'
+        )
+
+        await page.goto('https://www.airbnb.com/rooms/647559808158441027', {
+            waitUntil: 'networkidle2',
+        })
+
+        await clickNextOnCalendar(page)
+        await clickNextOnCalendar(page)
+        await getCalenderDays(page).then(days => calendarDays.push(...days))
+
+        for (let i = 0; i < 9; i++) {
+            await clickNextOnCalendar(page)
+            await getCalenderDays(page).then(days => calendarDays.push(...days))
+            calendarDays = removeDuplicates(calendarDays)
+        }
+
+        // TODO Remove
+        calendarDays.map(d => functions.logger.debug(new Date(d.date || '')))
+    } catch (e) {
+        functions.logger.error(e)
+    } finally {
+        if (browser) {
+            await browser.close()
+        }
+
+        functions.logger.info('Done with scrape')
     }
 
-    // TODO Remove
-    calendarDays.map(d => functions.logger.debug(new Date(d.date || '')))
-  } catch (e) {
-    functions.logger.error(e)
-  } finally {
-    if (browser) {
-      await browser.close()
-    }
-
-    functions.logger.info('Done with scrape')
-  }
-
-  return bookings
+    return bookings
 }
 
 export default scrapeQuarteira
