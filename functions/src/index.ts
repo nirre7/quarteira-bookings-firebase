0af324840f278b7763243517bd8e5f3d432b0254
Niclas Söderström <niclas.soderstrom@gmp-systems.com>
2023-01-24 09:01:04 +0100

    Fixed and updated functions config

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,23 +2,28 @@ import * as functions from 'firebase-functions'
 import scrapeQuarteria from './scrape-quarteria'
 import {createBookingsFromCalenderDays, saveBookings} from './upsert-bookings-service'
 
-exports.scrape = functions
+// TODO only for dev
+// exports.scrape = functions
+//     .runWith({
+//         timeoutSeconds: 120,
+//         memory: '512MB' || '2GB',
+//     })
+//     .region('europe-west3')
+//     .https.onRequest(async (req, res) => {
+//         const calendarDays = await scrapeQuarteria()
+//         let bookings = createBookingsFromCalenderDays(calendarDays)
+//         bookings = await saveBookings(bookings)
+//         res.type('html').send(bookings.map(b => b.start).join('<br>'))
+//     })
+
+exports.scrapingSchedule = functions
     .runWith({
         timeoutSeconds: 120,
         memory: '512MB' || '2GB',
     })
     .region('europe-west3')
-    .https.onRequest(async (req, res) => {
-        const calendarDays = await scrapeQuarteria()
-        let bookings = createBookingsFromCalenderDays(calendarDays)
-        bookings = await saveBookings(bookings)
-        res.type('html').send(bookings.map(b => b.start).join('<br>'))
-    })
-
-exports.scrapingSchedule = functions
-    .region('europe-west3')
     .pubsub
-    .schedule('06:00')
+    .schedule('every day 06:00')
     .timeZone('Europe/Stockholm')
     .onRun(async context => {
         const calendarDays = await scrapeQuarteria()
