542ba20f7e01f1732a6fcd17caff27ae2bebf4c6
Niclas Söderström <niclas.soderstrom@gmp-systems.com>
2023-02-04 21:53:00 +0100

    Added try/catch

--- functions/src/send-message-to-devices.ts
+++ functions/src/send-message-to-devices.ts
@@ -1,16 +1,21 @@
 import {firestore, messaging} from 'firebase-admin'
 import {DeviceToken} from './device-tokens'
 import {format} from 'date-fns'
+import * as functions from 'firebase-functions'
 
 async function sendMessageToDevices(numberOfNewBookings: number) {
     if (numberOfNewBookings > 0) {
         const deviceToken = await getToken()
-        await messaging().sendToDevice(deviceToken.tokens, {
-            data: {
-                numberOfNewBookings: numberOfNewBookings.toString(),
-                added: format(new Date(), 'yyyy-MM-dd'),
-            },
-        })
+        try {
+            await messaging().sendToDevice(deviceToken.tokens, {
+                data: {
+                    numberOfNewBookings: numberOfNewBookings.toString(),
+                    added: format(new Date(), 'yyyy-MM-dd'),
+                },
+            })
+        } catch (e) {
+            functions.logger.error(e, 'Could not send message to device')
+        }
     }
 }
 
