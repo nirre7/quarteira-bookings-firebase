f30b329af50aaee6ff0382d2bd536ab44cab9488
Niclas Söderström <niclas.soderstrom@gmp-systems.com>
2023-02-04 15:00:08 +0100

    Fixed formatting error and added check

--- functions/src/send-message-to-devices.ts
+++ functions/src/send-message-to-devices.ts
@@ -1,15 +1,17 @@
-import {messaging, firestore} from 'firebase-admin'
+import {firestore, messaging} from 'firebase-admin'
 import {DeviceToken} from './device-tokens'
 import {format} from 'date-fns'
 
 async function sendMessageToDevices(numberOfNewBookings: number) {
-    const deviceToken = await getToken()
-    await messaging().sendToDevice(deviceToken.tokens, {
-        data: {
-            numberOfNewBookings: numberOfNewBookings.toString(),
-            added: format(new Date(), 'YYYY-MM-DD'),
-        },
-    })
+    if (numberOfNewBookings > 0) {
+        const deviceToken = await getToken()
+        await messaging().sendToDevice(deviceToken.tokens, {
+            data: {
+                numberOfNewBookings: numberOfNewBookings.toString(),
+                added: format(new Date(), 'yyyy-MM-dd'),
+            },
+        })
+    }
 }
 
 async function getToken(): Promise<DeviceToken> {
